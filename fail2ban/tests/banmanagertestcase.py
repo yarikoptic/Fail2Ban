ac66df7de427dadf02478bd1e5a05cd1145a09d2
Alexander Koeppe <format_c@online.de>
2016-04-08 20:39:33 +0200

    move IPAddr related code to dedicated source file ipaddr.py

--- fail2ban/tests/banmanagertestcase.py
+++ fail2ban/tests/banmanagertestcase.py
@@ -28,7 +28,7 @@ import unittest
 
 from ..server.banmanager import BanManager
 from ..server.ticket import BanTicket
-from ..server.filter import IPAddr
+from ..ipaddr import IPAddr
 
 
 class AddFailure(unittest.TestCase):
